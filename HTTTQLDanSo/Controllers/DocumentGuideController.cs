80971e17b89a0b916ecab00a1285bdb825a920b6
loileq <lequangloi0909@gmail.com>
2023-12-26 10:53:05 +0700

    Add some document guide

---
+++ HTTTQLDanSo/Controllers/DocumentGuideController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace HTTTQLDanSo.Controllers
+{
+    public class DocumentGuideController : Controller
+    {
+        // GET: DocumentGuide
+        public ActionResult A0()
+        {
+            return View();
+        }
+        public ActionResult InformationReceipt()
+        {
+            return View();
+        }
+        public ActionResult YearlyCompare()
+        {
+            return View();
+        }
+    }
+}
